bb731c9632d4a86124b39a1d8707bae997addd85
aachman98 <aachman98@gmail.com>
2017-04-12 20:51:51 +0530

    Fragments Working

--- app/src/main/java/com/example/punyaaachman/albus/HomeActivity.java
+++ app/src/main/java/com/example/punyaaachman/albus/HomeActivity.java
@@ -10,6 +10,8 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.punyaaachman.albus.Fragments.ProfileFragment;
+import com.example.punyaaachman.albus.Fragments.ScanFragment;
+import com.example.punyaaachman.albus.Fragments.WalletFragment;
 
 public class HomeActivity extends AppCompatActivity
 {
@@ -20,13 +22,17 @@ public class HomeActivity extends AppCompatActivity
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item)
         {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
             switch (item.getItemId())
             {
                 case R.id.navigation_profile:
+                    fragmentTransaction.replace(R.id.content, new ProfileFragment()).commit();
                     return true;
                 case R.id.navigation_scan:
+                    fragmentTransaction.replace(R.id.content, new ScanFragment()).commit();
                     return true;
                 case R.id.navigation_wallet:
+                    fragmentTransaction.replace(R.id.content, new WalletFragment()).commit();
                     return true;
             }
             return false;
